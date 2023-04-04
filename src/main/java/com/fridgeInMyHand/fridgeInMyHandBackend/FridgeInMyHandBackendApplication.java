c8a41780ddc030ab8c7b0dfc2f0e152565d3e609
2480258 <swh8059@naver.com>
2023-04-04 16:09:18 +0900

    initial commit

---
+++ src/main/java/com/fridgeInMyHand/fridgeInMyHandBackend/FridgeInMyHandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fridgeInMyHand.fridgeInMyHandBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeInMyHandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeInMyHandBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fridgeInMyHand/fridgeInMyHandBackend/FridgeInMyHandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fridgeInMyHand.fridgeInMyHandBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeInMyHandBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
