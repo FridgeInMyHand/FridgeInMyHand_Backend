e1b731ffbe7b6ca2d90a9ee18731f50584485ad6
Yoojung Choi <101849662+eqvyoo@users.noreply.github.com>
2023-05-29 22:14:04 +0900

    Revert "Feature/food"

--- src/main/java/com/fridgeInMyHand/fridgeInMyHandBackend/FridgeInMyHandBackendApplication.java
+++ src/main/java/com/fridgeInMyHand/fridgeInMyHandBackend/FridgeInMyHandBackendApplication.java
@@ -2,8 +2,6 @@ package com.fridgeInMyHand.fridgeInMyHandBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 
 @SpringBootApplication
 public class FridgeInMyHandBackendApplication {
