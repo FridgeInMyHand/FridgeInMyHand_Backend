5c3459d68b1fd576e2f2724365189bf40b4e78f8
eqvyoo <ys07106@gmail.com>
2023-06-02 12:33:33 +0900

    revert food code

---
+++ src/main/java/com/fridgeInMyHand/fridgeInMyHandBackend/food/repository/FoodRepository.java
@@ -0,0 +1,12 @@
+package com.fridgeInMyHand.fridgeInMyHandBackend.food.repository;
+
+import com.fridgeInMyHand.fridgeInMyHandBackend.food.entity.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+
+    @Override
+    void deleteAll();
+
+    Food findByFoodName(String foodName);
+}
